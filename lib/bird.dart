ec7507d43659d769bc5c3558df180d800755c130
kei hasegawa <kei.hasegawa@gmail.com>
2022-03-14 10:49:50 +0900

    remove unused image

--- lib/bird.dart
+++ lib/bird.dart
@@ -18,7 +18,7 @@ class Bird extends SpriteComponent {
   double _deltaTime = 0;
 
   Bird(this._random) {
-    sprite = Sprite(Flame.images.fromCache(ImagePath.birdMove),
+    sprite = Sprite(Flame.images.fromCache(ImagePath.bird),
         srcPosition: Vector2(_count * defaultSize, _state * defaultSize),
         srcSize: Vector2.all(defaultSize));
     size = Vector2.all(defaultSize);
@@ -38,7 +38,7 @@ class Bird extends SpriteComponent {
         _state = (_state + 1 + _random.nextInt(2)) % 3;
       }
       _count = (_count + 1) % 2;
-      sprite = Sprite(Flame.images.fromCache(ImagePath.birdMove),
+      sprite = Sprite(Flame.images.fromCache(ImagePath.bird),
           srcPosition: Vector2(_count * defaultSize, _state * defaultSize),
           srcSize: Vector2.all(defaultSize));
     }
